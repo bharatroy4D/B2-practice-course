307641cee92d0e06114ff3e6036df2d2411217f2
bharatroy4D <bharatroy1005@gmail.com>
2026-05-08 10:07:06 +0600

    queue implement of enqueue , dequeue, front,etc is apply

--- src/queueOfArray.ts
+++ src/queueOfArray.ts
@@ -9,5 +9,29 @@ class Queue {
     dequeue(): number | undefined {
         return this.items.shift();
     }
-    
+    // see first element
+    front(): number | undefined {
+        return this.items[0]
+    }
+    // empty check
+    isEmpty(): boolean {
+        return this.items.length === 0;
+    }
+    // total size
+    size(): number {
+        return this.items.length;
+    }
 }
+
+// create queue
+const queue = new Queue();
+// add element
+queue.enqueue(10)
+queue.enqueue(20)
+queue.enqueue(30)
+console.log(queue.items);
+// dequeue, front, isEmpty, size
+console.log(`remove first element: ${queue.dequeue()}`);
+console.log(`see front element: ${queue.front()}`);
+console.log(`empty element: ${queue.isEmpty()}`);
+console.log(`total size element: ${queue.size()}`);
