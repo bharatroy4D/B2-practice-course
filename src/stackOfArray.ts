fbaeb618896a4e196b2750c4ac9d46413ff5f3f0
bharatroy4D <bharatroy1005@gmail.com>
2026-05-08 08:39:19 +0600

    Stack of pop and peek, Implemention useing array

--- src/stackOfArray.ts
+++ src/stackOfArray.ts
@@ -1,8 +1,16 @@
 class Stack {
     items: number[] = [];
-    
+
     // add element
     push(element: number): void {
-        this.items.push(element)
+        this.items.push(element);
+    }
+    // remove last element
+    pop(): number | undefined {
+        return this.items.pop();
+    }
+    // peek top element
+    peek(): number | undefined {
+        return this.items[this.items.length - 1];
     }
 }
